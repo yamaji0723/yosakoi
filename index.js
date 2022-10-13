997e7b1902bc20443860c0f4125851b31d6d6628
山路奈々 <y.nana0723@gmail.com>
2022-10-13 23:15:16 +0900

    ファーストコミット

---
+++ index.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('.profile').click(function(){
+        $('#profile-contents').fadeIn();
+    });
+
+    $('.tojiru').click(function(){
+        $('#profile-contents').fadeOut();
+    });
+
+    $('.practice1').click(function(){
+        $('#practice-contents').fadeIn();
+    });
+
+    $('.tojiru').click(function(){
+        $('#practice-contents').fadeOut();
+    });
+
+    $('.charm').click(function(){
+        $('#charm-contents').fadeIn();
+    });
+    
+    $('.tojiru').click(function(){
+        $('#charm-contents').fadeOut();
+    })
+})
